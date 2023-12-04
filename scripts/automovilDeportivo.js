121b8941f4d3f57083dffcf97c3bd9acb5fd7baf
Alex <aleexsantos1509@gmail.com>
2023-12-04 09:12:44 +0100

    Creada clase automovilDeportivo con atributos y metodos

--- scripts/automovilDeportivo.js
+++ scripts/automovilDeportivo.js
@@ -1,3 +1,29 @@
 import { Vehiculo } from "./vehiculo";
 
-export class automovilDeportivo extends Vehiculo {}
+export class automovilDeportivo extends Vehiculo {
+    #potenciaMotor;
+
+    constructor(
+        marca,
+        modelo,
+        color,
+        anioFabricacion,
+        cilindrada,
+        potenciaMotor
+    ) {
+        super(marca, modelo, color, anioFabricacion, cilindrada);
+        this.#potenciaMotor = potenciaMotor;
+    }
+
+    activarMotorDeportivo() {
+        return "El modo deportivo ha sido activado.";
+    }
+
+    getPotenciaMotor() {
+        return this.#potenciaMotor;
+    }
+
+    setPotenciaMotor(potenciaMotor) {
+        this.#potenciaMotor = potenciaMotor;
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,13 @@
+/**
+ * Autor: Alejandro Santos Garcia
+ * Repositorio Github:
+ */
+
 import { Vehiculo } from "./vehiculo";
 import { automovilDeportivo } from "./automovilDeportivo";
 
 const resultadoContainer = document.createElement("div");
+
+function mostrarResultados(mensaje) {
+    resultadoContainer.appendChild(<p>mensaje</p>);
+}
