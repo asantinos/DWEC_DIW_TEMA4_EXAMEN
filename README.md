fbcfe6eb608fb9168685caef772ef248be9d5c7f
Alex <aleexsantos1509@gmail.com>
2023-12-04 12:08:16 +0100

    Agregados pseudoselectores en CSS y README

---
+++ README.md
@@ -0,0 +1,5 @@
+# EXAMEN TEMA 4 DWEC Y DIW
+
+**Autor**: Alejandro Santos Garcia
+
+**Repositorio Github**: https://github.com/asantinos/DWEC_DIW_TEMA4_EXAMEN
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 /**
  * Autor: Alejandro Santos Garcia
- * Repositorio Github:
+ * Repositorio Github: https://github.com/asantinos/DWEC_DIW_TEMA4_EXAMEN
  */
 
 import { Vehiculo } from "./vehiculo.js";
