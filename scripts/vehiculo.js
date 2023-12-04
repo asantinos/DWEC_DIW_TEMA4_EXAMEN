a153e986733ddd1092c3ba65db8855c11f3c1ec1
Alex <aleexsantos1509@gmail.com>
2023-12-04 08:59:35 +0100

    Creada clase Vehiculo con propiedades y métodos

--- scripts/vehiculo.js
+++ scripts/vehiculo.js
@@ -1,3 +1,83 @@
 export class Vehiculo {
+    #marca;
+    #modelo;
+    #color;
+    #anioFabricacion;
+    #cilindrada;
 
+    constructor(marca, modelo, color, anioFabricacion, cilindrada) {
+        this.#marca = marca;
+        this.#modelo = modelo;
+        this.#color = color;
+        this.#anioFabricacion = anioFabricacion;
+        this.#cilindrada = cilindrada;
+    }
+
+    mostrarDatos() {
+        let datosTexto = `
+            <h1>Datos del Vehículo</h1>
+            <p>Marca: ${this.#marca}</p>
+            <p>Modelo: ${this.#modelo}</p>
+            <p>Color: ${this.#color}</p>
+            <p>Año de fabricación: ${this.#anioFabricacion}</p>
+        `;
+
+        return datosTexto;
+    }
+
+    acelerar(velocidad) {
+        return `El vehículo ha acelerado hasta ${velocidad}km/h`;
+    }
+
+    arrancar() {
+        return "El vehículo ha arrancado.";
+    }
+
+    frenar() {
+        return "El vehículo ha frenado.";
+    }
+
+    getMarca() {
+        return this.#marca;
+    }
+
+    getModelo() {
+        return this.#modelo;
+    }
+
+    getColor() {
+        return this.#color;
+    }
+
+    getAnioFrabricacion() {
+        return this.#anioFabricacion;
+    }
+
+    getCilindrada() {
+        return this.#cilindrada;
+    }
+
+    setMarca(marca) {
+        this.#marca = marca;
+    }
+
+    setModelo(modelo) {
+        this.#modelo = modelo;
+    }
+
+    setColor(color) {
+        this.#color = color;
+    }
+
+    setAnioFrabricacion(anioFabricacion) {
+        this.#anioFabricacion = anioFabricacion;
+    }
+
+    setCilindrada(cilindrada) {
+        this.#cilindrada = cilindrada;
+    }
+
+    mostrarResultado(){
+        
+    }
 }
